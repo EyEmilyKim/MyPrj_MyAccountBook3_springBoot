5bd0d0ac9577bddcdab93fcaa59be4153748223b
EyEmilyKim surf <myue4555@gmail.com>
2023-05-14 19:23:13 +0900

    INITIAL : new project

---
+++ src/main/java/com/EyEmilyKim/MyAccountBook3Application.java
@@ -0,0 +1,13 @@
+package com.EyEmilyKim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountBook3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAccountBook3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EyEmilyKim/MyAccountBook3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EyEmilyKim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAccountBook3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
