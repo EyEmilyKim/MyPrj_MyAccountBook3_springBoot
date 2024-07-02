d2c262afa2e8bfff4ceb684ff3a9c8cd75f8ae96
EyEmilyKim surf <myue4555@gmail.com>
2024-07-02 15:28:28 +0900

    FEAT : backToHome() on tran(Add)/add

--- src/main/resources/static/js/tranAdd/makeTran.js
+++ src/main/resources/static/js/tranAdd/makeTran.js
@@ -4,3 +4,18 @@
 function alertJSloaded() {
 	alert("js loaded.")
 }
+
+/* ------------ form 취소 ------------ */
+
+function backToHome(){
+	if(confirm("취소하고 홈으로 돌아가시겠습니까?"))	
+		location.href="/index";
+	else { //안나가고 reset 시 html,css & js_value 수동 초기화(fm 입력 값은 자동 초기화)
+		//버튼 색 all초기화 - 수입,지출,현금,카드 
+		//결제수단 row 초기화 (기본:가려짐)
+		//카테고리 드롭다운 초기화
+		//func로 전달한 INEX 값 초기화.
+		return false;
+	}
+}
+
--- src/main/webapp/WEB-INF/view/tranAdd/add.jsp
+++ src/main/webapp/WEB-INF/view/tranAdd/add.jsp
@@ -7,7 +7,15 @@
 	<div class="contMain">
 		<h3>가계부 쓰기 화면입니다.</h3>
 
+		<form action="add" method="post" name="fm" onSubmit="return catchSub()">
 		
+	<!-- 8.form 등록/취소 -->
+			<div class="marginTop">
+				<input type="submit" value="등록"> 
+				<input type="reset" value="취소" onClick="backToHome()">
+			</div>
+			
+		</form>
 
 	</div>
 	<!-- contMain 끝 -->
