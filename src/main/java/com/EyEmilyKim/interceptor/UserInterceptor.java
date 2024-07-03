e7f969394f9fc97ef7713db530d6cf309a63c74b
EyEmilyKim surf <myue4555@gmail.com>
2024-07-03 14:01:44 +0900

    FEAT: set master when browsing without login on userInterceptor

--- src/main/java/com/EyEmilyKim/interceptor/UserInterceptor.java
+++ src/main/java/com/EyEmilyKim/interceptor/UserInterceptor.java
@@ -10,7 +10,9 @@ public class UserInterceptor implements HandlerInterceptor {
 
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
 		HttpSession session = req.getSession();
-		int userId = (int) session.getAttribute("USER_ID");
+		Integer userId = (Integer) session.getAttribute("USER_ID");
+		// 개발용 임시!! 로그인 안했으면 master로 간주
+		if(userId == null) userId = 1;
 		req.setAttribute("userId", userId);
 		return true; // 다음 인터셉터 또는 컨트롤러로 요청 전달
 	}
