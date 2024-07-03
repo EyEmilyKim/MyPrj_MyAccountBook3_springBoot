ccedec6c768674d13e91ab30f9672e4ac9528e9c
EyEmilyKim surf <myue4555@gmail.com>
2024-07-03 16:15:06 +0900

    FEAT : add favicon resourceHandler on webConfig

--- src/main/java/com/EyEmilyKim/config/WebConfig.java
+++ src/main/java/com/EyEmilyKim/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.EyEmilyKim.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.EyEmilyKim.interceptor.UserInterceptor;
@@ -12,4 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**");
 	}
+	
+	@Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/favicon.ico")
+              .addResourceLocations("classpath:/static/");
+  }
 }
