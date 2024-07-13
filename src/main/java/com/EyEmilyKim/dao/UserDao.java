96f67d4e2fe61e95de583b36737b0a530a88bf2e
EyEmilyKim surf <myue4555@gmail.com>
2024-07-13 21:00:48 +0900

    FIX : add missing @Mapper annotation on UserDao

--- src/main/java/com/EyEmilyKim/dao/UserDao.java
+++ src/main/java/com/EyEmilyKim/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.EyEmilyKim.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.EyEmilyKim.entity.User;
 
+@Mapper
 public interface UserDao {
 
 	User findByLid(String lid);
