9033f362fcc90947100650eda3cd8f4488b859b1
EyEmilyKim surf <myue4555@gmail.com>
2024-07-15 19:05:03 +0900

    FEAT : add default variable of mncrd, meth_code, cate_code for ON DELETE trigger

--- src/main/java/com/EyEmilyKim/entity/Method.java
+++ src/main/java/com/EyEmilyKim/entity/Method.java
@@ -15,7 +15,9 @@ public class Method {
 	private Integer user_id;
 	private Integer seqno;
 	private String mncrd;
-	private String meth_name;
 	private String meth_code;
+	private String meth_name;
+	private String default_mncrd; // null -> DB default
+	private String default_meth_code; // null -> DB default
 	
 }
