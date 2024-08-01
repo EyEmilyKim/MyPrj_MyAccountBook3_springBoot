4bbec9a5052a49460885de500aea1f0e27943db6
EyEmilyKim surf <myue4555@gmail.com>
2024-08-02 02:56:34 +0900

    FEAT : set Asia/Seoul TimeZone as default on main method (sync with RDS server)

--- src/main/java/com/EyEmilyKim/MyAccountBook3Application.java
+++ src/main/java/com/EyEmilyKim/MyAccountBook3Application.java
@@ -1,5 +1,7 @@
 package com.EyEmilyKim;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyAccountBook3Application {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(MyAccountBook3Application.class, args);
 	}
 
