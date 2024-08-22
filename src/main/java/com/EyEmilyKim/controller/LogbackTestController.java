418b22b7aa8743bfd3b43ef412b08786b9bf111b
EyEmilyKim surf <myue4555@gmail.com>
2024-08-22 16:21:12 +0900

    FEAT : apply LogUtil.printWithTimestamp() to logbackTestController

--- src/main/java/com/EyEmilyKim/controller/LogbackTestController.java
+++ src/main/java/com/EyEmilyKim/controller/LogbackTestController.java
@@ -2,22 +2,28 @@ package com.EyEmilyKim.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.EyEmilyKim.util.LogUtil;
 
 @RequestMapping("/logbackTest/")
 @RestController
 public class LogbackTestController {
 	
 	private final Logger logger = LoggerFactory.getLogger(LogbackTestController.class);
+	private final String message = "Hello logbackTest!";
 	
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String logbackTest() {
-		return "Hello logbackTest!";
+		LogUtil.printWithTimestamp(message);
+		return message;
 	}
 	
-	@RequestMapping("/log")
+	@GetMapping("/log")
 	public void log() {
+		LogUtil.printWithTimestamp(message+"/log");
 		logger.trace("Trace Log");
 		logger.debug("Debug Log");
 		logger.info("info Log");
