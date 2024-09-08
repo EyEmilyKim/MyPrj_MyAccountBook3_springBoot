538f4e61f2932bb09298141369c4352ec97a1d29
EyEmilyKim surf <myue4555@gmail.com>
2024-09-09 01:27:04 +0900

    FEAT : add return type as ResponseEntity, @ApiResponses on ApiTestCont > log()

--- src/main/java/com/EyEmilyKim/controller/ApiTestController.java
+++ src/main/java/com/EyEmilyKim/controller/ApiTestController.java
@@ -2,6 +2,8 @@ package com.EyEmilyKim.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import com.EyEmilyKim.util.LogUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RequestMapping("/api/v1/")
@@ -32,12 +36,17 @@ public class ApiTestController {
 	
 	@GetMapping("/log")
 	@Operation(summary = "서버 콘솔에 테스트 로그 출력")
-	public void log(
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "요청 성공. 반환 데이터 없음"),
+			@ApiResponse(responseCode = "400", description = "잘못된 요청"),
+	})
+	public ResponseEntity<String> log(
 			@Parameter(name = "level", 
 				description = "출력할 테스트 로그 레벨 - all / trace / debug / info / warn / error", 
 				example = "all")
 			@RequestParam String level) {
 		LogUtil.printWithTimestamp("ApiTestController > log() : "+level);
+		boolean isOK = true;
 		switch(level) {
 			case "all" : 
 				logger.trace("Trace Log");
@@ -61,8 +70,12 @@ public class ApiTestController {
 			case "error" : 
 				logger.error("Error Log");
 				break;
+			default: 
+				isOK = false;
+				break;
 		}
-	
+		if(isOK) return new ResponseEntity<>("정상적으로 처리되었습니다.", HttpStatus.NO_CONTENT);
+		else return new ResponseEntity<>("파라미터가 유효하지 않습니다.", HttpStatus.BAD_REQUEST);
 	}
 
 }
