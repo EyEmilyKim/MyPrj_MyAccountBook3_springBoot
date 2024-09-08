e5db0ef7552f5e5945f6f8d842144fba9bd2d6d1
EyEmilyKim surf <myue4555@gmail.com>
2024-09-08 23:22:46 +0900

    FEAT : add Swagger dependency & apply on LogBackTestController

--- src/main/java/com/EyEmilyKim/controller/LogbackTestController.java
+++ src/main/java/com/EyEmilyKim/controller/LogbackTestController.java
@@ -8,20 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.EyEmilyKim.util.LogUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RequestMapping("/logbackTest/")
 @RestController
+@Tag(name = "LogbackTest", description = "서버 측 로깅 확인용")
 public class LogbackTestController {
 	
 	private final Logger logger = LoggerFactory.getLogger(LogbackTestController.class);
 	private final String message = "Hello logbackTest!";
 	
 	@GetMapping("/hello")
+	@Operation(summary = "check RestController", description = "RestController 작동 확인")
 	public String logbackTest() {
 		LogUtil.printWithTimestamp(message);
 		return message;
 	}
 	
 	@GetMapping("/log")
+	@Operation(summary = "leave server side log in all level", description = "서버 콘솔에 레벨별 테스트 로그 남기기")
 	public void log() {
 		LogUtil.printWithTimestamp(message+"/log");
 		logger.trace("Trace Log");
