b6bf5a30cf2118f0f4bf37cda2dcbb9a4fea10ca
EyEmilyKim surf <myue4555@gmail.com>
2024-09-09 23:52:29 +0900

    TEST : add HomeControllerTest > testIndexGet_succ()

---
+++ src/test/java/com/EyEmilyKim/controller/HomeControllerTest.java
@@ -0,0 +1,38 @@
+package com.EyEmilyKim.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.EyEmilyKim.config.OperatingHoursConfig;
+import com.EyEmilyKim.interceptor.LoginInterceptor;
+import com.EyEmilyKim.service.UserService;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private UserService userService;
+	
+	@MockBean
+	private OperatingHoursConfig operatingHoursConfig;
+	@MockBean
+	private LoginInterceptor loginInterceptor;
+	
+	@Test
+	void testIndexGet_success() throws Exception {
+		mockMvc.perform(get("/index"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("root.index"));
+	}
+
+}
