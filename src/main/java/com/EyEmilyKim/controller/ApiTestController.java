711c9834f34b175515b322e8627dbecb715fc0cf
EyEmilyKim surf <myue4555@gmail.com>
2024-09-09 20:33:42 +0900

    FEAT : add @profile (disable on "prod") to ApiTestController

--- src/main/java/com/EyEmilyKim/controller/ApiTestController.java
+++ src/main/java/com/EyEmilyKim/controller/ApiTestController.java
@@ -2,6 +2,7 @@ package com.EyEmilyKim.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Profile("!prod") // 배포 환경에서는 비활성화
 @RequestMapping("/api/v1/")
 @RestController
 @Tag(name = "API Test", description = "자잘한 작동 확인용 엔드포인트")
