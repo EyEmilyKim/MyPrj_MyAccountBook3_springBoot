613aa9b93397e7af55380210e5876a4fd7b682a3
EyEmilyKim surf <myue4555@gmail.com>
2024-09-16 00:15:59 +0900

    FEAT : add api.properties, ApiProperties

---
+++ src/main/java/com/EyEmilyKim/config/properties/ApiProperties.java
@@ -0,0 +1,19 @@
+package com.EyEmilyKim.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Component
+@ConfigurationProperties(prefix = "api")
+public class ApiProperties {
+	
+	private String server_path;
+	
+}
