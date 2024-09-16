a24c3cb3c04c3eb5ac787633a8afe1477234257b
EyEmilyKim surf <myue4555@gmail.com>
2024-09-16 14:57:45 +0900

    FEAT : apply swagger to CategoryController

--- src/main/java/com/EyEmilyKim/controller/CategoryController.java
+++ src/main/java/com/EyEmilyKim/controller/CategoryController.java
@@ -17,8 +17,15 @@ import com.EyEmilyKim.entity.Category;
 import com.EyEmilyKim.service.CategoryService;
 import com.EyEmilyKim.util.LogUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RequestMapping("/set/category/")
 @Controller
+@Tag(name="view - 카테고리", description = "카테고리 페이지 그룹")
 public class CategoryController {
 	
 	@Autowired
@@ -30,6 +37,8 @@ public class CategoryController {
 		
 	/*-------- 카테고리 목록 --------*/
 	
+	@Operation(summary = "목록 페이지", description = "")
+	@ApiResponse(responseCode = "200", description = "정상적으로 페이지 반환됨")
 	@GetMapping("list")
 	public String list(HttpServletRequest req, Model model) {
 		LogUtil.printWithTimestamp("CategoryController > list() called");
@@ -46,8 +55,13 @@ public class CategoryController {
 	
 	/*-------- 카테고리 삭제 --------*/
 	
+	@Operation(summary = "삭제 페이지", description = "")
+	@ApiResponse(responseCode = "200", description = "정상적으로 페이지 반환됨")
 	@GetMapping("del")
-	public String del(String CCODE, Model model) {
+	public String del(
+			@Parameter(name = "CCODE", description = "카테고리 코드", example = "", required = true)
+			String CCODE, 
+			Model model) {
 		LogUtil.printWithTimestamp("CategoryController > del()@Get called");
 		System.out.println(CCODE);
 		
@@ -57,8 +71,18 @@ public class CategoryController {
 		return "set.category.del";
 	}
 	
+	@Operation(summary = "삭제 페이지", description = "")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "삭제 성공 알림, \n\n리다이렉트: 목록 페이지"),
+			@ApiResponse(responseCode = "500", description = "삭제 실패 알림, \n\n리다이렉트: 목록 페이지")
+	})
 	@PostMapping("del")
-	public String del(String CCODE, Model model, String CNAME) {
+	public String del(
+			@Parameter(name = "CCODE", description = "카테고리 코드", example = "", required = true)
+			String CCODE, 
+			@Parameter(name = "CNAME", description = "카테고리 이름 ** to_be_refactored **", example = "", required = true)
+			String CNAME,
+			Model model) {
 		LogUtil.printWithTimestamp("CategoryController > del()@Post called");
 		System.out.println(CCODE+" - "+CNAME);
 		
@@ -76,6 +100,8 @@ public class CategoryController {
 	
 	/*-------- 카테고리 추가 --------*/
 	
+	@Operation(summary = "추가 페이지", description = "")
+	@ApiResponse(responseCode = "200", description = "정상적으로 페이지 반환됨")
 	@GetMapping("add")
 	public String add(HttpServletRequest req, Model model) {
 		LogUtil.printWithTimestamp("CategoryController > add()@Get called");
@@ -90,8 +116,15 @@ public class CategoryController {
 		return "set.category.add";
 	}
 	
+	@Operation(summary = "추가 페이지", description = "")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "추가 성공 알림, \n\n리다이렉트: 목록 페이지"),
+			@ApiResponse(responseCode = "500", description = "추가 실패 알림, \n\n리다이렉트: 목록 페이지")
+	})	
 	@PostMapping("add")
-	public String add(@RequestParam Map<String,String> fm, HttpServletRequest req, Model model) {
+	public String add(
+			@RequestParam Map<String,String> fm, 
+			HttpServletRequest req, Model model) {
 		LogUtil.printWithTimestamp("CategoryController > add()@Post called");
 		int userId = (int) req.getAttribute("userId");
 		System.out.println("userId : "+userId);
@@ -113,8 +146,14 @@ public class CategoryController {
 	
 	/*-------- 카테고리 수정 --------*/ 
 	
+	@Operation(summary = "수정 페이지", description = "")
+	@ApiResponse(responseCode = "200", description = "정상적으로 페이지 반환됨")
 	@GetMapping("upd")
-	public String upd(HttpServletRequest req, String CCODE, Model model) {
+	public String upd(
+			@Parameter(name = "CCODE", description = "카테고리 코드", example = "", required = true)
+			String CCODE, 
+			HttpServletRequest req, 
+			Model model) {
 		LogUtil.printWithTimestamp("CategoryController > upd()@Get called");
 		int userId = (int) req.getAttribute("userId");
 		System.out.println("userId : "+userId);
@@ -128,8 +167,15 @@ public class CategoryController {
 		return "set.category.upd";
 	}
 	
+	@Operation(summary = "수정 페이지", description = "")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "수정 성공 알림, \n\n리다이렉트: 목록 페이지"),
+			@ApiResponse(responseCode = "500", description = "수정 실패 알림, \n\n리다이렉트: 목록 페이지")
+	})
 	@PostMapping("upd")
-	public String upd(@RequestParam Map<String,String> fm, Model model) {
+	public String upd(
+			@RequestParam Map<String,String> fm, 
+			Model model) {
 		LogUtil.printWithTimestamp("CategoryController > upd()@Post called");
 		for(Map.Entry<String,String> entry : fm.entrySet()) {
 			System.out.println(entry.getKey()+" : "+entry.getValue());
