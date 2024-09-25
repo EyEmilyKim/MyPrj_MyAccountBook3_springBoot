a83b93097f5485e01c9d2daf9a4cdc77c9eea4c3
EyEmilyKim surf <myue4555@gmail.com>
2024-09-26 00:16:42 +0900

    REFACTOR : modify CustomErrorController (change error messages from static String to Dynamic data)

--- src/main/java/com/EyEmilyKim/controller/CustomErrorController.java
+++ src/main/java/com/EyEmilyKim/controller/CustomErrorController.java
@@ -3,33 +3,39 @@ package com.EyEmilyKim.controller;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.EyEmilyKim.service.MessageService;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 	
 	private static final String ERROR_PATH = "/error";
 	
+	@Autowired
+	private MessageService messageService;
+	
 	@GetMapping(ERROR_PATH)
 	public String handleError(HttpServletRequest req, Model model) {
 		Object status = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		String referer = req.getHeader("Referer");
 		String nextUrl = (referer != null) ? referer : "/index";
-		String failMsg;
+		String errorMessage;
 		
 		if (status != null) {
 			Integer statusCode = Integer.valueOf(status.toString());
 			if (statusCode == 404) {
-				failMsg = "존재하지 않는 페이지입니다.";
+				errorMessage = messageService.getMessage("message-error", "msg.error.404");
 			} else if (statusCode == 500) {
-				failMsg = "서버 내부 오류가 발생했습니다.";
+				errorMessage = messageService.getMessage("message-error", "msg.error.500");
 			} else {
-				failMsg = "알 수 없는 오류가 발생했습니다.";
+				errorMessage = messageService.getMessage("message-error", "msg.error.else");
 			}
-			model.addAttribute("MSG", failMsg);
+			model.addAttribute("MSG", errorMessage);
 			model.addAttribute("URL", nextUrl);
 		}
 		
