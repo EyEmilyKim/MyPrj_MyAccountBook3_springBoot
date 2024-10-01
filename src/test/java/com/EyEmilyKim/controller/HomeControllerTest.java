aa6cd0186cba85bbb6dde91b1016511554a7c5aa
EyEmilyKim surf <myue4555@gmail.com>
2024-10-02 03:40:40 +0900

    TEST : fix HomeContTest, PlanContTest (change @WebMvcTest -> @SpringBootTest)

--- src/test/java/com/EyEmilyKim/controller/HomeControllerTest.java
+++ src/test/java/com/EyEmilyKim/controller/HomeControllerTest.java
@@ -10,7 +10,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +22,8 @@ import com.EyEmilyKim.interceptor.LoginInterceptor;
 import com.EyEmilyKim.service.MessageService;
 import com.EyEmilyKim.service.UserService;
 
-@WebMvcTest(HomeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class HomeControllerTest {
 	
 	@Autowired
