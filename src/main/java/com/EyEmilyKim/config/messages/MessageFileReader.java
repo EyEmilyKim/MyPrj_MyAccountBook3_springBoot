f4c3372a1e2c3573a7b13ea2bd9b944d12adee5a
EyEmilyKim surf <myue4555@gmail.com>
2024-10-03 01:44:56 +0900

    FIX : modify MessageFileReader.loadMessages() to the way able to resource file both in local and .war

--- src/main/java/com/EyEmilyKim/config/messages/MessageFileReader.java
+++ src/main/java/com/EyEmilyKim/config/messages/MessageFileReader.java
@@ -1,26 +1,26 @@
 package com.EyEmilyKim.config.messages;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageFileReader {
 	
-	private final String path = "src/main/resources/";
 	private final String suffix = ".txt";
 
 	public Map<String, String> loadMessages(String fileName) throws IOException {
 		Map<String, String> messages = new HashMap<>();
-		String target = path + fileName + suffix;
+		String target = fileName + suffix;
 		
-		try(Stream<String> lines = Files.lines(Paths.get(target))){
-			lines.forEach(line -> {
+		ClassPathResource resource = new ClassPathResource(target);
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+				br.lines().forEach(line -> {
 				String [] keyValue = line.split("=", 2);
 				if(keyValue.length == 2) {
 					messages.put(keyValue[0].trim(), keyValue[1].trim());
